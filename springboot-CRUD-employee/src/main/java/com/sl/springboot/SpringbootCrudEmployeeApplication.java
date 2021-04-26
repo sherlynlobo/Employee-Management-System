237d6e1e1f771d18c2f2484b60f64041d9d3b7cb
sherlynlobo <sherlynlobo26@gmail.com>
2021-04-26 18:46:41 -0400

    Created MVC architecture structure.

---
+++ springboot-CRUD-employee/src/main/java/com/sl/springboot/SpringbootCrudEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.sl.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudEmployeeApplication.class, args);
+	}
+
+}
