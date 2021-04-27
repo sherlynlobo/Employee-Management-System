caa8eae951d041720aafeb9eb0d683deea37ac1e
sherlynlobo <sherlynlobo26@gmail.com>
2021-04-27 03:14:25 -0400

    List Employees feature

---
+++ springboot-CRUD-employee/src/main/java/com/sl/springboot/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.sl.springboot.service;
+
+import java.util.List;
+
+import com.sl.springboot.model.Employee;
+
+public interface EmployeeService {
+	
+	List<Employee> getAllEmployees();
+
+}
---
+++ springboot-CRUD-employee/src/main/java/com/sl/springboot/service/EmployeeServiceImpl.java
@@ -0,0 +1,22 @@
+package com.sl.springboot.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sl.springboot.model.Employee;
+import com.sl.springboot.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@Override
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
+
+}
